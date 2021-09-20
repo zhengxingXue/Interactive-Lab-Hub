003181dfd4fa55b2b0367c9d77833b5ba0910a48
zhengxingXue <zx252cornell@gmail.com>
2021-09-20 16:26:56 -0400

    part d

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -64,7 +64,7 @@ while True:
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
-    #Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
+    # Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
     time_string = time.strftime("%m/%d/%Y %H:%M:%S")
     dx, dy = font.getsize(time_string)
     x = (width - dx) / 2
